3e39c8920390d189f84a49324fd478a31e4fbdf2
maxamed-cali <haamcali@gmail.com>
2025-05-10 14:24:33 +0300

    add for websoket

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"bank/db"
 	"bank/routes"
-
+	"bank/websocket"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+// Start WebSocket dispatcher
+    websocket.StartDispatcher()
 
 	r:=gin.Default();
 	db.Connect()
